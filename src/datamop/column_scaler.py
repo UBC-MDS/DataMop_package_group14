cccafb1c41a72e0042efcfd486c3fe9dac50a385
Sepehr Heydarian <sepehrhey@gmail.com>
2025-01-17 09:44:29 -0800

    update docstring for column_scaler

--- src/datamop/column_scaler.py
+++ src/datamop/column_scaler.py
@@ -10,21 +10,22 @@ def column_scaler(data, column, method="minmax", new_min=0, new_max=1, inplace=T
         The name of the numeric column to scale.
     method: str
         The method used for scaling. Options include:
-            - 'minmax': Scales values between 'new_min' and 'new_max', used as default method.
-            - 'standard': Scales values with mean of 0 and standard deviation of 1.
+            - `minmax`: Scales values between `new_min` and `new_max`, used as default method.
+            - `standard`: Scales values with mean of 0 and standard deviation of 1.
     new_min: float
         The lower boundary value for min-max scaling. Default value is 0.
     new_max: float
         The upper boundary value for min-max scaling. Default value is 1. 
     inplace: bool
-        When equals 'True', the function replaces the original column with the scaled values,
-        when equals 'False', the function adds the new scaled column to the DataFrame,
-        Default is 'True'
+        If `True` the column is replaced with new scaled column.
+        If `False` the original column is retained and the new scaled column is added to the dataframe with title `<column-name>-scaled`.
+        Default is True
     
     Returns
     --------
     pandas.DataFrame
-        A copy of the DataFrame with the scaled column.
+        A copy of the DataFrame with the scaled column replacing the original column if `inplace` is set to True.
+        If `inplace` is set to False, the copy of DataFrame is returned with the new scaled column added, keeping the original column.
 
     Raises
     ------
@@ -36,9 +37,7 @@ def column_scaler(data, column, method="minmax", new_min=0, new_max=1, inplace=T
     Examples
     --------
     >>> import pandas as pd
-    >>> df = pd.DataFrame({
-            "price: [25, 50, 75]
-            }")
+    >>> df = pd.DataFrame({"price": [25, 50, 75]})
     >>> df_scaled = column_scaler(df, column = 'price', method='minmax', new_min=0, new_max=1)
     >>> print(df_scaled)
             price
@@ -47,3 +46,4 @@ def column_scaler(data, column, method="minmax", new_min=0, new_max=1, inplace=T
             0.75
 
     """
+    pass
